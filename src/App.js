fb6c9015deaf466a3def8050bc3e00cfbe5ab5b8
Aaron Perel <a.a.ron.perel9@gmail.com>
2024-07-11 19:27:04 -0400

    added files for reading history and recommendations and added route for recommendations page

--- src/App.js
+++ src/App.js
@@ -50,6 +50,7 @@ function App() {
             <Route path="/book/:id" element={<Book setIsOpen={setIsOpen} isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} isOpen={isOpen} firstUserName={firstUserName} setFirstUserName={setFirstUserName} user={user}/>} />
             <Route path="/ebooksearch" element={<EBookSearch setIsOpen={setIsOpen} isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} isOpen={isOpen} firstUserName={firstUserName} setFirstUserName={setFirstUserName} user={user}/>} />
             <Route path="/settings" element={<Settings setIsOpen={setIsOpen} isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} isOpen={isOpen} firstUserName={firstUserName} setFirstUserName={setFirstUserName} user={user}/>} />
+            {/* <Route path="/recommendations" element={<RecommendationsPage setIsOpen={setIsOpen} isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} isOpen={isOpen} firstUserName={firstUserName} setFirstUserName={setFirstUserName} user={user}/>} /> */}
           </Routes>
         </Suspense>
       </BrowserRouter>
