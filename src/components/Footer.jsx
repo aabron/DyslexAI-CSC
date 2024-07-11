75d6d867dd79600d4dc6d4dd24ee5097e19ab90d
Aaron Perel <a.a.ron.perel9@gmail.com>
2024-07-11 19:52:57 -0400

    Update Footer.jsx
    Changed "Made By students" to "Made by <our names>"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="header-animation p-6 text-white sticky font-reddit">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <p className="text-center md:text-left mb-4 md:mb-0">{new Date().getFullYear()} DyslexAI. Made By Students.</p>
+        <p className="text-center md:text-left mb-4 md:mb-0">{new Date().getFullYear()} DyslexAI. Made By Saba Begum, Aaron Perel, Ishmael Gonzalez and Mahbubur Khan.</p>
         <a
           href="https://github.com/aabron/DyslexAI-CSC"
           target="_blank"
